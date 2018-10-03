f79924b38bf0e1ee0453d508f6259af3942850d3
erpel <philipp@copythat.de>
2018-10-03 21:21:08 +0200

    Add bash completion

---
+++ lib/lmcadm/commands/completion.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module LMCAdm #:nodoc:
+  desc 'Output shell commands to enable completion in bash'
+  arg_name :shell
+  command :completion do |completion|
+    completion.action do |_o, _g, args|
+      raise 'only bash supported at the moment' unless
+          [nil, 'bash'].include? args.first
+      puts <<~'BASH'
+        #source this into your bash (via ~/.bashrc or other means)
+        complete -F _lmcadm_completions lmcadm
+        function _lmcadm_completions()
+        {
+            COMPREPLY=(`lmcadm help -c "${COMP_WORDS[@]:1}"`)
+        }
+        BASH
+    end
+  end
+end
