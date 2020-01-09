0470f59d2fdd37a8a85a79b8875b6a3a5ea56854
Philipp Erbelding <philipp.erbelding@lancom.de>
2020-01-09 16:03:30 +0100

    Add option to get the scripting state

--- lib/lmcadm/commands/maintenance.rb
+++ lib/lmcadm/commands/maintenance.rb
@@ -14,14 +14,22 @@ module LMCAdm
     maintenance.arg_name 'UUID', [:required]
     maintenance.desc 'Enable scripting for an account'
     maintenance.command :scripting do |scr|
+      scr.desc 'Change the state of scripting'
       scr.switch :enable
+      scr.desc 'Get the state of scripting'
+      scr.switch :get
       scr.action do |global_options, options, args|
         raise "No account UUID specified" if args.length < 1
         root_account = LMC::Account.get LMC::Account::ROOT_ACCOUNT_UUID
         cloud = LMC::Cloud.instance
         cloud.auth_for_account root_account
-        result = cloud.put ['cloud-service-config', 'configroot', 'accounts', args.first, 'scriptauthority'], options[:enable]
-        raise "error - unexpected result" unless result.body == options[:enable]
+        if options[:get]
+          result = cloud.get ['cloud-service-config', 'configroot', 'accounts', args.first, 'scriptauthority']
+          puts result.body
+        else
+          result = cloud.put ['cloud-service-config', 'configroot', 'accounts', args.first, 'scriptauthority'], options[:enable]
+          raise "error - unexpected result" unless result.body == options[:enable]
+        end
       end
     end
 
