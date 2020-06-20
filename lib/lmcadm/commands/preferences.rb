4b7a523aa73e37fe105ef763fe438db5ce51b43d
erpel <philipp@copythat.de>
2020-06-20 21:35:12 +0200

    Add preferences commands

---
+++ lib/lmcadm/commands/preferences.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+module LMCAdm #:nodoc:
+  desc 'Manipulate preferences'
+  command :preferences do |preferences|
+    preferences.desc 'Get last account data'
+    preferences.arg_name 'accounts'
+    preferences.command :lastaccounts do |la|
+      la.action do |g, _o, args|
+        cloud = LMC::Cloud.new(g[:cloud_host], g[:user], g[:password])
+        self_ui = cloud.preferences [:principals, :self, :ui]
+        if args.empty?
+          ids = self_ui.get 'lastAccountIds'
+          accounts = ids.map do |id|
+            LMC::Account.get id
+          end
+          accounts.each { |a|
+            puts a.summary
+          }
+        else
+          account_ids = args.map { |arg|
+            LMC::Account.get_by_uuid_or_name(arg).id
+          }
+          puts account_ids
+          puts self_ui.put 'lastAccountIds', account_ids
+        end
+      end
+    end
+  end
+end
+
