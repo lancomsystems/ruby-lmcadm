24b6b079ed9e61cad4949c5e2b55530b8f1986f7
erpel <philipp@copythat.de>
2021-05-26 22:36:30 +0200

    drop dev dependencies

--- makeocragemfile.rb
+++ makeocragemfile.rb
@@ -4,7 +4,7 @@ GEMFILE = 'ocra_gemfile'
 # Add gems from gemspec.
 ocra_gemfile = File.new(GEMFILE, 'w')
 File.open("lmcadm.gemspec",'r') do |file|
-  file.each { |line| ocra_gemfile.write "gem #{$1}\n" if line =~ /spec.add_.*dependency (.+)/ }
+  file.each { |line| ocra_gemfile.write "gem #{$1}\n" if line =~ /spec.add_runtime_dependency (.+)/ }
 end
 ocra_gemfile.close
 
